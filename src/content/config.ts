ad9124d46e93119e0d040e977b74920434cd643d
lgirs <l_girs@hotmail.com>
2025-08-19 01:18:21 +0300

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const pages = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    tags: z.array(z.string()).default([])
+  }),
+});
+
+export const collections = { pages };
